a1cb07fef9284d862f4438135d02546173c6c410
Antoan Milkov <tonyboy0777@gmail.com>
2016-04-07 20:34:45 +0300

    Update m160407_112730_token_expire.php
    PosgreSQL alter column default value case

--- migrations/m160407_112730_token_expire.php
+++ migrations/m160407_112730_token_expire.php
@@ -3,15 +3,17 @@
 use nkostadinov\user\models\Token;
 use yii\db\Migration;
 
-class m160407_112730_token_expire extends Migration
-{
-    public function up()
-    {
-        $this->alterColumn(Token::tableName(), 'expires_on', $this->integer()->defaultValue(0));
+class m160407_112730_token_expire extends Migration {
+
+    public function up() {
+        if ($this->db->driverName === 'pgsql') {
+            $this->alterColumn(Token::tableName(), 'expires_on', 'SET DEFAULT 0');
+        } else {
+            $this->alterColumn(Token::tableName(), 'expires_on', $this->integer()->defaultValue(0));
+        }
     }
 
-    public function down()
-    {
+    public function down() {
         $this->alterColumn(Token::tableName(), 'expires_on', $this->integer()->notNull());
     }
 
